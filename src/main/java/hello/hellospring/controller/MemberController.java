75d277d3295bb92b7bf1adfeeff435cfb50b4222
whtjddbs <jsy_1201@naver.com>
2023-12-27 01:30:30 +0900

    회원 관리 - 메모리 저장

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,9 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -20,7 +22,7 @@ public class MemberController {
 
     @GetMapping("/members/new")
     public String createForm() {
-        return "mebmers/createMemberForm";
+        return "members/createMemberForm";
     }
 
     @PostMapping("/members/new")
@@ -28,16 +30,17 @@ public class MemberController {
         Member member = new Member();
         member.setName(form.getName());
 
+        System.out.println("member = " + member.getName());
         memberService.join(member);
 
         return "redirect:/";
     }
 
     @GetMapping("/members")
-    public ModelAndView getList() {
-        ModelAndView mv = new ModelAndView();
-        
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
 
-        return mv;
+        return "members/memberList";
     }
 }
