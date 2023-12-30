81796142b110767fc5dd6c959bc45092d7ae368b
whtjddbs <jsy_1201@naver.com>
2023-12-31 01:50:39 +0900

    1. 스프링 통합 테스트 2. 스프링 DB 접근 기술 활용    1) 스프링 JdbcTemplate    2) JPA - hibernate    3) 스프링 데이터 JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,34 +1,52 @@
 package hello.hellospring;
 
 import hello.hellospring.domain.Member;
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+    /* JPA 사용을 위한 EntityManager Bean 생성 및 DI */
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+    /* jdbc 연결을 위한 dataSource Bean 생성 및 DI */
+//    private DataSource dataSource;
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource); //메모리 저장에서 DB저장으로 변경
-    }
+//        return new JdbcMemberRepository(dataSource); //메모리 저장에서 DB저장으로 변경
+//        return new JdbcTemplateMemberRepository(dataSource); // jdbcTemplate 사용하도록 변경
+//        return new JpaMemberRepository(em); // JPA 사용
+//    }
+
 }
